a126ce1ded8b0754ca5f8f744dc9a44575b48bc4
psolitech <psolitechjs@gmail.com>
2025-01-17 10:28:17 -0500

    Update MadLibs.js

--- MadLibs.js
+++ MadLibs.js
@@ -124,4 +124,70 @@ function reset() {
     "<br><button onclick='stories(1)'>Story 1</button>"+
     "<br><button onclick='stories(2)'>Story 2</button>"+
     "<br><button onclick='stories(3)'>Story 3</button>";
+
+  story1 = ["This weekend I am going camping with ",
+    ". I packed my lantern, sleeping bag, and ",
+    ". I am so ",
+    " to ",
+    " in a tent. I am ",
+    " we might see a ",
+    ", they are kind of dangerous. We are going to hike, fish, and ",
+    ". I have heard that the ",
+    " lake is great for ",
+    ". Then we will ",
+    " hike through the forest for ",
+    " ",
+    ". If I see a ",
+    " ",
+    " while hiking, I am going to bring it home as a pet! At night we will tell ",
+    " ",
+    " stories and roast ",
+    " around the campfire!!"];
+
+  story2 = ["It was about ",
+    " ",
+    " ago when I came to the hospital in a ",
+    ". The hospital is a/an ",
+    " place, there are a lot of ",
+    " ",
+    " here. There are nurses here who have ", 
+    " ",
+    ". If someone wants to come into my room I told them that they have to ",
+    " first. I have decorated my room with ",
+    " ",
+    ". Today a doctor came into my room and they were wearing a ",
+    " on their ",
+    ". I heard that all doctors ",
+    " ",
+    " every day for breakfast. The most ",
+    " thing about being in the hospital is the ",
+    " ",
+    "!"]
+  
+  story3 = ["Dear ",
+    ", I am writing to you from a ",
+    " castle in an enchanted forest. I found myself here one day after going for a ride on a ",
+    " ",
+    " in ",
+    ". There are ",
+    " ",
+    " and ",
+    " ",
+    " here! In the ",
+    " there is a pool full of ",
+    ". I fall asleep each night on a ",
+    " of ",
+    " and dream of ",
+    " ",
+    ". It feels as though I have lived here for ",
+    " ",
+    ". I hope one day you can visit, although the only way to get here now is ",
+    " on a ",
+    " ",
+    "!!"]; 
+  
+  entries1 = ["Proper Noun (Person's Name)","Noun","Adjective (Feeling)","Verb","Adjective (Feeling)","Animal","Verb","Color","Verb (ending in ing)","Adverb (ending in ly)","Number","Measure of Time","Color","Animal","Number","Silly Word","Noun"];
+  entries2 = ["Number", "Measure of time", "Mode of Transportation", "Adjective", "Adjective", "Noun", "Color", "Part of the Body", "Verb", "Number", "Noun", "Noun", "Part of the Body", "Verb", "Noun", "Adjective", "Silly Word", "Noun"];
+  entries3 = ["Proper Noun (Person’s Name)", "Adjective", "Color", "Animal", "Place", "Adjective", "Magical Creature (Plural)", "Adjective", "Magical Creature (Plural)", "Room in a House", "Noun", "Noun", "Noun", "Adjective", "Noun (Plural)", "Number", "Measure of time", "Verb (ending in ing)", "Adjective", "Noun"];
+
 }
